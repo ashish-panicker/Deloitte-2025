801a5b35d03f223cf81fb2c10aedc14ca84dbc10
ashish-panicekr <ashish.s.panicekr@proton.me>
2025-10-01 15:09:34 +0530

    Day 3: JDBC With DML

--- Demos/java-jdbc/src/main/java/org/example/Main.java
+++ Demos/java-jdbc/src/main/java/org/example/Main.java
@@ -27,9 +27,9 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        String userName = "root";
-        String password = "password";
-        String url = "jdbc:mysql://localhost:3306/dbname?useSSL=false";
+        String userName = "";
+        String password = "";
+        String url = "";
 
         // These 2 steps are optional with latest mysql driver
 //         Driver d = new Driver(); // Create driver instance
@@ -46,16 +46,33 @@ public class Main {
 //        );
 
 
-
         // Obtain a connection
         Connection connection = DriverManager.getConnection(url, userName, password);
 
+        final String insertQuery = """
+                    insert into customers (customerNumber, customerName, phone, city, creditLimit)
+                    values (?, ?, ?, ?, ?)
+                """;
+        // To execute incomplete queries
+        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+
+        // Provide values for the placeholders
+        preparedStatement.setInt(1, 111);
+        preparedStatement.setString(2, "Raj");
+        preparedStatement.setString(3, "8989787845");
+        preparedStatement.setString(4, "New Delhi");
+        preparedStatement.setDouble(5, 50000);
+
+        // For DML queries use executeUpdate()
+        int rowsUpdated = preparedStatement.executeUpdate();
+
         // Execute a query
-        final String query = "select * from customers";
+        final String selectQuery = "select * from customers";
         // Create a Statement object
         Statement statement = connection.createStatement();
         // Execute the query and obtain the result set
-        ResultSet resultSet = statement.executeQuery(query);
+        ResultSet resultSet = statement.executeQuery(selectQuery);
+
         while (resultSet.next()) {
             int customerNumber      = resultSet.getInt("customerNumber");
             String customerName     = resultSet.getString("customerName");
